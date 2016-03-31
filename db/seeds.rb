6b7e422e8c45e9e93937142c1877ef9049830e86
zhuleijia <zhuleijia@gmail.com>
2016-03-31 18:27:08 -0400

    modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
 user1 = User.create(email: "zhuleijia@gmail.com", password: "53080949", real_name: "Lei Corp")
 guest = User.create(email: "guest@guest.com", password: "guestguest", real_name:"guest")
 
+Location.destroy_all
 new_york = Location.create(city: "New York")
 new_port = Location.create(city: "New Port")
 
+Job.destroy_all
 job1 = Job.create(
 		title: "Engineer",
 		jobtype:	"full_time",
