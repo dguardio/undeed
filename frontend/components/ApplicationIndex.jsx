0ee1746e8cb108158800cf572b93ca44afe530b1
zhuleijia <zhuleijia@gmail.com>
2016-04-23 15:07:10 -0400

    add jobstore listerner

--- frontend/components/ApplicationIndex.jsx
+++ frontend/components/ApplicationIndex.jsx
@@ -14,7 +14,7 @@ var AppIndexItem = require('./AppIndexItem');
 var ApplicationIndex = React.createClass({
 	getInitialState: function () {
     return {
-      jobId : this.props.params.jobId,
+      job : {},
       apps : [],
       user : {},
     };
@@ -23,18 +23,28 @@ var ApplicationIndex = React.createClass({
   componentDidMount: function (){
     this.applicationStoreToken = ApplicationStore.addListener(this._onChangeApp);
     this.sessionStoreToken = SessionStore.addListener(this._onChangeSession);
+    this.jobStoreToken = JobStore.addListener(this._onChangeJob);
     ApiUtil.fetchCurrentUser();
-    ApiUtil.fetchAppsWithJobID(parseInt(this.state.jobId));
+		var jobId = parseInt(this.props.params.jobId);
+		ApiUtil.fetchSingleJob(jobId);
+    ApiUtil.fetchAppsWithJobID(jobId);
   },
 
   componentWillUnmount: function() {
     this.applicationStoreToken.remove();
     this.sessionStoreToken.remove();
+    this.jobStoreToken.remove();
   },
   _onChangeApp: function () {
 		this.setState({
       apps: ApplicationStore.all(),
      });
+	},
+  _onChangeJob: function () {
+		this.setState({
+      job: JobStore.all()[0],
+     });
+
 	},
   _onChangeSession: function () {
 		this.setState({
@@ -43,6 +53,7 @@ var ApplicationIndex = React.createClass({
 	},
 
   render: function() {
+				 console.log(this.state.job);
     var apps = this.state.apps.map(function(app) {
 
       return <AppIndexItem key={app.id} app={app}/>;
@@ -52,7 +63,7 @@ var ApplicationIndex = React.createClass({
         <div>
 
     			<Link className="logo-link" to={"/"}><Logo /></Link>
-					<h2>Jobs Posted by me ({this.state.user.real_name})</h2>
+					<h2>Applications received from Job ({this.state.job.title})</h2>
           <div className="search-results">
             {apps}
           </div>
